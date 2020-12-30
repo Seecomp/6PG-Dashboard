70de5a6ff191ebb25c7d9ee3075de3fd129ad5fa
Seecomp <73800126+Seecomp@users.noreply.github.com>
2020-12-30 15:00:34 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   endpoint: '/api',
-  discordInvite: 'https://discord.gg/24tycau',
-  githubURL: 'https://github.com/theADAMJR/6PG-Dashboard',
+  discordInvite: 'https://discord.gg/D9s8h4K5rp',
+  githubURL: 'https://github.com/Seecomp/6PG-Dashboard',
   version: 'v0.0.1a'
 };
